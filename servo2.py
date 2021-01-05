15b2562bedf4941d927eb793d6a8215b46993840
Antoine Coulon <antoine.coulon@epita.fr>
2021-01-05 17:40:21 +0100

    sd

---
+++ servo2.py
@@ -0,0 +1,11 @@
+import pantilthat
+import sys
+import time
+
+pan_angle = int(sys.argv[1])
+tilt_angle = int(sys.argv[2])
+
+if __name__ == "__main__":
+    pantilthat.tilt(tilt_angle)
+    pantilthat.pan(pan_angle)
+    time.sleep(0.5)
