18d4f7aef0365370495bcc6f82828de99e856ec1
Antoine Coulon <antoine.coulon@epita.fr>
2021-01-06 17:32:11 +0100

    test_pth

--- src/cpppth/check_pth.cpp
+++ src/cpppth/check_pth.cpp
@@ -67,49 +67,95 @@ int lsleep(long ms)
 
 void init_pth(pantilthat *pth)
 {
-    unsigned char i = 0;
+    unsigned char i = 0, i1 = 0, i2 = 0;
+    int ii = 0, ii1 = 0, ii2 = 0;
+    int err = -1;
 
-    // May rise an exception if the system is unable to create an instance
-    printf("\n[INFO] checking the Pan-Tilt HAT module...\n\n");
+    try {
+        // May rise an exception if the system is unable to create an instance
 
-    // Initial configuration
-    if (!pth->setup()) {
-        cerr << "[FATAL] Could not initialize Pan-Tilt HAT module. Aborting.\n"
+        printf("\n[INFO] checking the Pan-Tilt HAT module...\n\n");
+
+        // Initial configuration
+        if (!pth->setup()) {
+            cerr
+                << "[FATAL] Could not initialize Pan-Tilt HAT module. Aborting.\n"
+                << endl;
+            goto error;
+        }
+        // Reset the PTH device
+        if (!pth->i2c_write_byte(REG_CONFIG, /*00001100*/ 0x0C))
+            goto error;
+        lsleep(FIFTYMILLIS);
+        // Check that Servo 1 and Servo 2 motors are disabled, that lights are
+        // enabled and that Light Mode is WS2812 (Config register == 00001100 ==
+        // 0x0c)
+        printf("\tChecking servos and light initial status...");
+        fflush(stdout);
+        if (!pth->i2c_read_byte(REG_CONFIG, &i)) {
+            printf(NOK);
+            goto error;
+        } else if (i != /*00001100*/ 0x0c) {
+            printf(NOK);
+            goto error;
+        }
+        printf(OK);
+        // This configuration is known to be fool-proof
+        printf("\tSetting known good config...");
+        fflush(stdout);
+        if ((!pth->servo_enable(1, 1)) || (!pth->servo_enable(2, 1)) ||
+            (!pth->servo_pulse_min(1, 575)) ||
+            (!pth->servo_pulse_max(1, 2300)) ||
+            (!pth->servo_pulse_min(2, 575)) ||
+            (!pth->servo_pulse_max(2, 2300))) {
+            printf(NOK);
+            goto error;
+        }
+        printf(OK);
+        // Set pan then tilt position to zero
+        for (i = 1; i < 3; i++) {
+            printf("\tSetting servo %d to 0 degrees...", i);
+            fflush(stdout);
+            pth->set_servo(i, 0);
+            printf(OK);
+        }
+        // The PTH must be able to move (-89, +89) along both axis
+        printf("\tChecking ability to move...");
+        fflush(stdout);
+        for (ii = -89; ii < 90; ii++) {
+            pth->set_servo(1, ii);
+            pth->set_servo(2, ii);
+            lsleep(50);
+            ii1 = pth->get_servo(1);
+            ii2 = pth->get_servo(2);
+            if ((ii1 != ii) || (ii2 != ii)) {
+                printf(NOK);
+                goto error;
+            }
+        }
+        // Set PTH head back to its zero position
+        pth->set_servo(1, 0);
+        pth->set_servo(2, 0);
+        lsleep(ONESECOND);
+        printf(OK);
+
+        // Getting to this point asserts no error occurred
+        err = 0;
+        printf("\n[SUCCESS] Pan-Tilt HAT module is fully operational.\n\n");
+
+    error:
+        // Get rid of PTH object and frees memory.
+        if (pth)
+            delete pth;
+
+    } catch (exception &e) {
+        cerr << "[FATAL] could not create Pan-Tilt HAT instance class : "
+             << e.what() << "Aborting.\n"
              << endl;
-        goto error;
+        exit(-1);
     }
-    // Reset the PTH device
-    if (!pth->i2c_write_byte(REG_CONFIG, /*00001100*/ 0x0C))
-        goto error;
-    lsleep(FIFTYMILLIS);
-    // Check that Servo 1 and Servo 2 motors are disabled, that lights are
-    // enabled and that Light Mode is WS2812 (Config register == 00001100 ==
-    // 0x0c)
-    printf("\tChecking servos and light initial status...");
-    fflush(stdout);
-    if (!pth->i2c_read_byte(REG_CONFIG, &i)) {
-        printf(NOK);
-        goto error;
-    } else if (i != /*00001100*/ 0x0c) {
-        printf(NOK);
-        goto error;
-    }
-    printf(OK);
-    // This configuration is known to be fool-proof
-    printf("\tSetting known good config...");
-    fflush(stdout);
-    if ((!pth->servo_enable(1, 1)) || (!pth->servo_enable(2, 1)) ||
-        (!pth->servo_pulse_min(1, 575)) || (!pth->servo_pulse_max(1, 2300)) ||
-        (!pth->servo_pulse_min(2, 575)) || (!pth->servo_pulse_max(2, 2300))) {
-        printf(NOK);
-        goto error;
-    }
-    printf(OK);
 
-error:
-    // Get rid of PTH object and frees memory.
-    if (pth)
-        delete pth;
+    return err;
 }
 
 pantilthat *get_pth()
@@ -136,7 +182,6 @@ int check_pth(pantilthat *pth)
 
     try {
         // May rise an exception if the system is unable to create an instance
-        //pantilthat *pth = new pantilthat();
 
         printf("\n[INFO] checking the Pan-Tilt HAT module...\n\n");
 
@@ -234,10 +279,8 @@ int check_pth(pantilthat *pth)
 
 int main()
 {
-    //pantilthat *pth = new pantilthat();
     pantilthat *pth = get_pth();
     //check_pth(pth);
-    //pantilthat *pth = get_pth();
 
     init_pth(pth);
 
@@ -249,7 +292,7 @@ int main()
     lsleep(300);
     pth->set_servo(2, 0);
     printf(OK);
-    */
+*/
 
     int ii = 0, ii1 = 0, ii2 = 0;
 
