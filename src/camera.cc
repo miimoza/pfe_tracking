6ebb409cf5ce4d242fdfa775776617370dfdf514
Antoine Coulon <antoine.coulon@epita.fr>
2021-01-06 17:02:56 +0000

    camera: Open /dev/video0

--- src/camera.cc
+++ src/camera.cc
@@ -2,6 +2,10 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
+
+using namespace std;
+using namespace cv;
+
 cv::VideoCapture getVideoCapture()
 {
     cv::VideoCapture cap(0);
@@ -17,12 +21,52 @@ cv::VideoCapture getVideoCapture()
 cv::Mat getImg(cv::VideoCapture cap)
 {
     cv::Mat img;
-    bool bSucess = cap.read(img);
-    if (!bSucess)
-    {
-        std::cout << "Cannot read a frame from picam" << std::endl;
-        //return;
-    }
+    cap.read(img);
 
     return img;
+}
+
+int main ()
+{
+    /*cv::VideoCapture cap = getVideoCapture();
+      for (;;)
+      {
+      cv::imshow("debug", getImg(cap));
+      }
+      return 0;*/
+
+    Mat frame;
+    //--- INITIALIZE VIDEOCAPTURE
+    VideoCapture cap;
+    // open the default camera using default API
+    // cap.open(0);
+    // OR advance usage: select any API backend
+    int deviceID = 0;             // 0 = open default camera
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+    // open selected camera using selected API
+    cap.open(deviceID, apiID);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    //--- GRAB AND WRITE LOOP
+    cout << "Start grabbing" << endl
+        << "Press any key to terminate" << endl;
+    for (;;)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        cap.read(frame);
+        // check if we succeeded
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        // show live and wait for a key with timeout long enough to show images
+        imshow("Live", frame);
+        if (waitKey(5) >= 0)
+            break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
 }
