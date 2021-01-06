1fd8b5e52f627714f2d7a51868500d62ec8598df
Antoine Coulon <antoine.coulon@epita.fr>
2021-01-06 16:19:50 +0000

    camera: Get frames from picam

--- src/camera.cc
+++ src/camera.cc
@@ -1,1 +1,28 @@
-// FILE
+#include <iostream>
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+cv::VideoCapture getVideoCapture()
+{
+    VideoCapture cap(0);
+    if (!cap.isOpened())
+    {
+        std::cout << "Cannot open picam" << std::endl;
+        return;
+    }
+
+    return cap;
+}
+
+cv::Mat getImg(cv::VideoCapture cap)
+{
+    cv::Mat img;
+    bool bSucess = cap.read(img);
+    if (!bSucess)
+    {
+        std::cout << "Cannot read a frame from picam" << std::endl;
+        return;
+    }
+
+    return img;
+}
